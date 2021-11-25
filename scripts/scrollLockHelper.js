95ace3382a608cba3429585b759ae572e61bb22b
Slobodan Subotic <slobodan.subotic@vivifyideas.com>
2021-11-25 09:55:38 +0100

    Added variables for colors, and changed scroll fix.

--- scripts/scrollLockHelper.js
+++ scripts/scrollLockHelper.js
@@ -2,10 +2,13 @@ let scrollPosition = 0;
 
 const lockScroll = () => {
   scrollPosition = window.pageYOffset;
-  document.body.style.overflow = "hidden";
-  document.body.style.position = "fixed";
-  document.body.style.top = `-${scrollPosition}px`;
-  document.body.style.width = "100%";
+  // On older iOS page flashes when scroll locks, so this adds a delay for that until animation is finished.
+  setTimeout(() => {
+    document.body.style.overflow = "hidden";
+    document.body.style.position = "fixed";
+    document.body.style.top = `-${scrollPosition}px`;
+    document.body.style.width = "100%";
+  }, 300)
 };
 
 const unlockScroll = () => {
